b0a0a08715c5c690a47489d8b7e30d38bed6ff9b
Jake Anderson <anderson.jake.scott@gmail.com>
2018-11-16 03:04:57 -0700

    added directions

--- class-project/class-project/MapViewController.swift
+++ class-project/class-project/MapViewController.swift
@@ -13,19 +13,31 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var map: MKMapView!
     var searchActive : Bool = false
-    
-    var manager:CLLocationManager!
+    var currentLocation=CLLocation();
+    var userPlacemark=MKPlacemark();
+    var currentTransportType = MKDirectionsTransportType.automobile
+    var currentRoute: MKRoute?
 
+    var manager:CLLocationManager!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        map.delegate = self
         
         manager = CLLocationManager()
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+        self.map.showsUserLocation = true
+        if #available(iOS 9.0, *) {
+            self.map.showsCompass = true
+            self.map.showsScale = true
+            self.map.showsTraffic = true
+        }
+        
         // Do any additional setup after loading the view.
     }
     
@@ -47,11 +59,11 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        //userLocation - there is no need for casting, because we are now using CLLocation object
-        
         let userLocation:CLLocation = locations[0]
-        self.showCurrentLocation(location: userLocation)
-        
+        self.currentLocation = userLocation;
+        let span = MKCoordinateSpan(latitudeDelta: 0.075, longitudeDelta: 0.075)
+        let region = MKCoordinateRegion(center: self.currentLocation.coordinate, span: span)
+        self.map.setRegion(region, animated: false)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -65,6 +77,7 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
     }
     
     func search(querey: String){
+        self.map.removeAnnotations(self.map.annotations)
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = querey
         request.region = map.region
@@ -74,31 +87,16 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
             guard let response = response else {
                 return
             }
-            print( response.mapItems )
             var matchingItems:[MKMapItem] = []
             matchingItems = response.mapItems
             self.showOnMap(matchingItems: matchingItems)
         }
     }
     
-    func showCurrentLocation(location: CLLocation){
-        let geoCoder = CLGeocoder()
-        // Geocode Location
-        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
-            if error != nil {
-                print("Geocode failed: \(error!.localizedDescription)")
-            } else if placemarks!.count > 0 {
-                let placemark = placemarks![0]
-                let location = placemark.location
-                let coords = location!.coordinate
-                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-                let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
-                self.map.setRegion(region, animated: true)
-
-            }
-        }
-    }
     
+    func mapViewDidFinishRenderingMap(_ mapView: MKMapView, fullyRendered: Bool){
+            self.search(querey: "book store")
+    }
     
     func showOnMap(matchingItems: [MKMapItem]){
         
@@ -115,18 +113,10 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
                     print("Geocode failed: \(error!.localizedDescription)")
                 } else if placemarks!.count > 0 {
                     let placemark = placemarks![0]
-                    print("=====================================================")
-                    print(place)
-                    print("=====================================================")
-                    let location = placemark.location
-                    let coords = location!.coordinate
-                    let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-                    let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
-                    self.map.setRegion(region, animated: true)
                     let ani = MKPointAnnotation()
                     ani.coordinate = placemark.location!.coordinate
                     ani.title = place.name
-                    ani.subtitle = placemark.locality
+                    ani.subtitle = String(placemark.location!.distance(from: self.currentLocation))
                     self.map.addAnnotation(ani)
                     
                 }
@@ -134,5 +124,81 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
         }
         
     }
+    func getPlacemark(location: CLLocation) -> MKPlacemark{
+        let geoCoder = CLGeocoder()
+        // Geocode Location
+        var placemark = MKPlacemark()
+        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error != nil {
+                print("Geocode failed: \(error!.localizedDescription)")
+            } else if placemarks!.count > 0 {
+//                placemark = placemarks![0]
+                let coordinate = placemarks![0].location!.coordinate
+                placemark = MKPlacemark(coordinate: coordinate)
+            }
+        }
+        return placemark
+    }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView)
+    {
+        let place = MKPlacemark(coordinate: (view.annotation?.coordinate)!)
+        self.showDirections(place: place)
+    }
     
+    func showDirections(place: MKPlacemark)
+    {
+        
+        let x: CLLocationCoordinate2D = self.currentLocation.coordinate
+        let userPlacemark = MKPlacemark(coordinate: x)
+        // get the directions
+        let directionRequest = MKDirections.Request()
+        
+        // Set the source and destination of the route
+        let sourcePM = MKPlacemark(placemark: userPlacemark)
+        directionRequest.source = MKMapItem(placemark: sourcePM)
+        
+        //directionRequest.source = MKMapItem.forCurrentLocation()
+        
+        let destinationPM = MKPlacemark(placemark: place)
+        directionRequest.destination = MKMapItem(placemark: destinationPM)
+        
+        directionRequest.transportType = currentTransportType
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate { (routeResponse, routeError) -> Void in
+            
+            guard let routeResponse = routeResponse else {
+                if let routeError = routeError {
+                    print("Error: \(routeError)")
+                }
+                
+                return
+            }
+            
+            let route = routeResponse.routes[0]
+            print("Printing route")
+            for step in route.steps {
+                print(step.instructions)
+            }
+            
+            
+            self.currentRoute = route
+            self.map.removeOverlays(self.map.overlays)
+            self.map.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            let big = MKMapRect(x: rect.minX - 2000, y: rect.minY - 2000, width: rect.width + 4000 , height: rect.height + 4000)
+            self.map.setRegion(MKCoordinateRegion(big), animated: true)
+        }
+        
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = (currentTransportType == .automobile) ? UIColor.blue : UIColor.orange
+        renderer.lineWidth = 4.0
+        
+        return renderer
+    }
 }

249f2f259636eb977fe303e519a1228e6d0440e1
Jake Anderson <anderson.jake.scott@gmail.com>
2018-11-16 00:06:39 -0700

    getting all place marks in map

--- class-project/class-project/MapViewController.swift
+++ class-project/class-project/MapViewController.swift
@@ -9,25 +9,64 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
+class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var map: MKMapView!
     var searchActive : Bool = false
     
+    var manager:CLLocationManager!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        self.search()
+    class func isLocationServiceEnabled() -> Bool {
+        if CLLocationManager.locationServicesEnabled() {
+            switch(CLLocationManager.authorizationStatus()) {
+            case .notDetermined, .restricted, .denied:
+                return false
+            case .authorizedAlways, .authorizedWhenInUse:
+                return true
+            default:
+                print("Something wrong with Location services")
+                return false
+            }
+        } else {
+            print("Location services are not enabled")
+            return false
+        }
     }
     
-    func search(){
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //userLocation - there is no need for casting, because we are now using CLLocation object
+        
+        let userLocation:CLLocation = locations[0]
+        self.showCurrentLocation(location: userLocation)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.search(querey: self.searchBar.text!)
+    }
+    
+    func search(querey: String){
         let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = self.searchBar.text!
+        request.naturalLanguageQuery = querey
         request.region = map.region
         let search = MKLocalSearch(request: request)
         
@@ -38,40 +77,60 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
             print( response.mapItems )
             var matchingItems:[MKMapItem] = []
             matchingItems = response.mapItems
-            let place = matchingItems[0].placemark
-            let lat = place.location?.coordinate.latitude
-            let long = place.location?.coordinate.longitude
-            self.showOnMap(lati: Double(lat!), longi: Double(long!))
+            self.showOnMap(matchingItems: matchingItems)
         }
     }
     
- 
-
-    func showOnMap(lati: Double , longi: Double){
-        let location = CLLocation(latitude: lati, longitude: longi)
-        
+    func showCurrentLocation(location: CLLocation){
+        let geoCoder = CLGeocoder()
         // Geocode Location
-        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+        geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error != nil {
+                print("Geocode failed: \(error!.localizedDescription)")
+            } else if placemarks!.count > 0 {
+                let placemark = placemarks![0]
+                let location = placemark.location
+                let coords = location!.coordinate
+                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                self.map.setRegion(region, animated: true)
+
+            }
+        }
+    }
+    
+    
+    func showOnMap(matchingItems: [MKMapItem]){
+        
+        for i in 1...matchingItems.count - 1{
+            let place = matchingItems[i].placemark
+            let lat = Double((place.location?.coordinate.latitude)!)
+            let long = Double((place.location?.coordinate.longitude)!)
+            let location = CLLocation(latitude: lat, longitude: long)
+            
+            let geoCoder = CLGeocoder()
+            // Geocode Location
+            geoCoder.reverseGeocodeLocation(location) { (placemarks, error) in
                 if error != nil {
                     print("Geocode failed: \(error!.localizedDescription)")
                 } else if placemarks!.count > 0 {
                     let placemark = placemarks![0]
+                    print("=====================================================")
+                    print(place)
+                    print("=====================================================")
                     let location = placemark.location
                     let coords = location!.coordinate
-                    print(location)
-                    
-                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                    let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
                     let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
                     self.map.setRegion(region, animated: true)
                     let ani = MKPointAnnotation()
                     ani.coordinate = placemark.location!.coordinate
-                    ani.title = placemark.locality
-                    ani.subtitle = placemark.subLocality
-                    
+                    ani.title = place.name
+                    ani.subtitle = placemark.locality
                     self.map.addAnnotation(ani)
                     
-                    
                 }
+            }
         }
         
     }

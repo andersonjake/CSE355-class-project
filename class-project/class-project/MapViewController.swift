acc1b50ca86093abad0e37a5662de885f6de6bcf
Jake Anderson <anderson.jake.scott@gmail.com>
2018-11-15 23:28:53 -0700

    got map Search to work

--- class-project/class-project/MapViewController.swift
+++ class-project/class-project/MapViewController.swift
@@ -7,24 +7,73 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var map: MKMapView!
+    var searchActive : Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.search()
     }
-    */
+    
+    func search(){
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = self.searchBar.text!
+        request.region = map.region
+        let search = MKLocalSearch(request: request)
+        
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            print( response.mapItems )
+            var matchingItems:[MKMapItem] = []
+            matchingItems = response.mapItems
+            let place = matchingItems[0].placemark
+            let lat = place.location?.coordinate.latitude
+            let long = place.location?.coordinate.longitude
+            self.showOnMap(lati: Double(lat!), longi: Double(long!))
+        }
+    }
+    
+ 
 
+    func showOnMap(lati: Double , longi: Double){
+        let location = CLLocation(latitude: lati, longitude: longi)
+        
+        // Geocode Location
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+                if error != nil {
+                    print("Geocode failed: \(error!.localizedDescription)")
+                } else if placemarks!.count > 0 {
+                    let placemark = placemarks![0]
+                    let location = placemark.location
+                    let coords = location!.coordinate
+                    print(location)
+                    
+                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                    let region = MKCoordinateRegion(center: placemark.location!.coordinate, span: span)
+                    self.map.setRegion(region, animated: true)
+                    let ani = MKPointAnnotation()
+                    ani.coordinate = placemark.location!.coordinate
+                    ani.title = placemark.locality
+                    ani.subtitle = placemark.subLocality
+                    
+                    self.map.addAnnotation(ani)
+                    
+                    
+                }
+        }
+        
+    }
+    
 }
